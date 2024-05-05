a09ac1d093dada1097b76ea5b1a05f612e011ebc
Ben Wiz <bwisialowski@gmail.com>
2024-05-05 14:48:28 -0500

    components

--- src/benwiz/spotify_tools/views/search.cljs
+++ src/benwiz/spotify_tools/views/search.cljs
@@ -1,11 +1,17 @@
 (ns benwiz.spotify-tools.views.search
   (:require
    ["@mui/icons-material/Clear" :default ClearIcon]
+   ["@mui/icons-material/Add" :default AddIcon]
+   ["@mui/icons-material/Remove" :default RemoveIcon]
+   ["@mui/joy/Input" :default Input]
    ["@mui/icons-material/Equalizer" :default EqualizerIcon]
    ["@mui/joy/AspectRatio" :default AspectRatio]
-   ["@mui/joy/Autocomplete" :default Autocomplete]
-   ["@mui/joy/AutocompleteOption" :default AutocompleteOption]
+   ["@mui/joy/ButtonGroup" :default ButtonGroup]
+   ["@mui/joy/Button" :default Button]
+   ["@mui/joy/IconButton" :default IconButton]
    ["@mui/joy/Card" :default Card]
+   ["@mui/joy/Slider" :default Slider]
+   ["@mui/joy/TextField" :default TextField]
    ["@mui/joy/CardContent" :default CardContent]
    ["@mui/joy/CardOverflow" :default CardOverflow]
    ["@mui/joy/CircularProgress" :default CircularProgress]
@@ -28,6 +34,61 @@
    [re-frame.core :as rf]
    [reagent.core :as r]))
 
+(defn discrete-control [k [min max] default]
+  (let [v        (rf/subscribe [::subs/db k])
+        on-click (fn [f]
+                   (fn [_]
+                     (rf/dispatch [::events/edit-db assoc k (f (or @v default))])))]
+    [:> ButtonGroup {:orientation "vertical"}
+     [:> IconButton {:onClick (on-click inc)} [:> AddIcon]]
+     ;; [:> Button {:disabled true} (or @v default)]
+     [:> Input {:variant  "outlined"
+                :type "number"
+                :value    (or (str @v) default)
+                :onChange (fn [^js e]
+                            (when-some [new-v (some-> (.. e -target -value) not-empty js/parseInt)]
+                              (rf/dispatch [::events/edit-db assoc k
+                                            new-v
+                                            #_(cond ;; can't do this because I need to type the first digit of a large number
+                                                (> new-v max) max
+                                                (< new-v min) min
+                                                :else         new-v)])))
+                :sx       #js {:maxWidth "100px"}}]
+     [:> IconButton {:onClick (on-click dec)} [:> RemoveIcon]]]))
+
+(defn range-slider [[low-k min] [high-k max]]
+  (let [low          (rf/subscribe [::subs/db low-k])
+        high         (rf/subscribe [::subs/db high-k])
+        on-change    (fn [k default]
+                       (fn [^js e]
+                         (let [new-v (.. e -target -value)]
+                           (rf/dispatch [::events/edit-db assoc k (or new-v default)]))))]
+    [:> Stack {:direction "row" :spacing 3}
+     ;; TODO I need a label in here
+     [discrete-control :search/low [min max] min]
+     [:> Slider {:sx                #js {:flexGrow 1}
+                 :track             false
+                 :value             #js [(or @low min) (or @high max)]
+                 :min               min
+                 :max               max
+                 :marks             #js [#js {:value min :label (str min)}
+                                         (let [mid (/ max 2)] #js {:value mid :label (str mid)})
+                                         #js {:value max :label (str max)}]
+                 :getAriaLabel      (fn [] "Range Slider Name")
+                 :valueLabelDisplay "auto" ;; "on" "off"
+                 :onChange          (fn [^js _e [low high]]
+                                      (rf/dispatch [::events/edit-db assoc :search/low (or low min)])
+                                      (rf/dispatch [::events/edit-db assoc :search/high (or high max)]))}]
+     [discrete-control :search/high [min max] max]]))
+
+(defn app []
+  (let []
+    [:<>
+     ;; [spotify/searcher]
+     [range-slider
+      [:search/low 10]
+      [:search/high 90]]]))
+
 (defn panel []
   (let [token (rf/subscribe [::subs/db :spotify/token])]
     [:> Stack {:spacing    2
@@ -35,10 +96,10 @@
      [c/header {:title "Search and Filter"}]
      (if @token
        [:> Card {:variant "outlined"
-                   :sx      {:alignSelf "center"
-                             :maxWidth  "800px"
-                             :width     "100%"}}
-          [:> CardContent nil
-           "hi"
-           [spotify/searcher]]]
+                 :sx      {:alignSelf "center"
+                           :maxWidth  "800px"
+                           :width     "100%"}}
+        [:> CardContent nil
+
+         [app]]]
        [spotify/button])]))
